91513431178770d39b41031fbb266ca9273cdc8b
Tim <tetchel@gmail.com>
2017-05-30 02:06:41 -0400

    add validation

--- js/file-picker.js
+++ js/file-picker.js
@@ -1,17 +1,16 @@
 "use strict";
 
-// Maps questions to answer arrays
-var answersMap = {};
-var answerTextAreasMap = {};
+// The first index is the question # (zero-indexed), the second index 
+// is the nth answer column corresponding to that question
+var answersArray = [];
 
-/*
+
 window.onbeforeunload = function() {
     if(isEmptyObject(answersMap)) {
         return "Are you sure you want to leave? Your current quiz will be lost!";
     }
     return;
 }
-*/
 
 function filePicked(files) {
     var file = files[0], 
@@ -26,11 +25,11 @@ function filePicked(files) {
     reader.readAsText(file, "UTF-8");
     
     reader.onload = function (e) {
-        $("#file-upload-indicator").html(file.name).show();
+        $("#file-display").html(file.name).show();
         buildTable(e.target.result);
     };
     reader.onerror = function (e) {
-        $("#file-upload-indicator").html(e.target.result).show()
+        $("#file-display").html(e.target.result).show()
     };
 }
 
@@ -66,17 +65,23 @@ function buildTable(csvText) {
     
     for (i = 0; i < lines.length - 1; i++) {
         // array of cells for this line - [0] is the question, rest are answers
-        var line = lines[i].slice(0,-1).split(",");
+        var line = lines[i].split(",");
         
         // Map the question to the array of answers
-        answersMap[line[0]] = line.slice(1, line.length);
+        answersArray[i] = line.slice(1, line.length);
         
         // The row starts off with the question
         var row = "<tr><td>" + line[0] + "</td>";
         var j;
         // Then we add one text box cell per answer
         for(j = 1; j < line.length; j++) {
-            row += "<td><textarea class=\"answer-textarea\"></textarea></td>";
+            if(line[j] && line[j].length !== 0) {
+                // The ID corresponds to the index of the expected answer in answersArray
+                // answersArray[1][1] will have id 1_1
+                var id = i + "_" + (j-1);
+                row += "<td><textarea class=\"answer-textarea\" id=\"" + id + "\">" + 
+                    "</textarea>&nbsp;</td>";
+            }
         }
         row += "</tr>";
         output.push(row);
@@ -107,16 +112,58 @@ function shuffleArray(array) {
 }
 
 function validate() {
-    var tableContents = {};
-    $("#table-div table tr").each(function() {
-        var row = [];
-        var tableData = $(this).find('td');
-        if(tableData.length > 0) {
-            // Slice to ignore the question (first td)
-            tableData.slice(1, tableData.length).each(function() {
-                row.push($(this).text());
-            });
-            
+    $("body").css("cursor", "progress");
+    
+    // Delete previous validation results
+    $(".correct-answer").remove();
+    $(".wrong-answer").remove();
+    
+    var answerTextBoxes = $(".answer-textarea");
+    // track # of questions right/wrong
+    var correct = 0, total = 0;
+        
+    answerTextBoxes.each(function() {
+        var id = $(this).attr('id');
+        // Recall the element's ID is its answer index i_j
+        var iIndex = id.substr(0, id.indexOf("_"));
+        var jIndex = id.substr(id.indexOf("_") + 1);
+        
+        var text = $(this).val();        
+        var answer = answersArray[iIndex][jIndex];
+        
+        // Write either Correct or Wrong under each answer box
+        var toAppend = "";
+        if(answer.trim().toLowerCase().localeCompare(text.toLowerCase().trim()) === 0) {
+            toAppend = "<span class=\"correct-answer\">&#10004; Correct" + "</span>";
+            correct++;
         }
+        else {
+            toAppend = "<span class=\"wrong-answer\">&#10006; Expected: " + answer + "</span>";
+        }
+        
+        total++;        
+        var parent = $(this).parent();
+        parent.append(toAppend);
     });
+    
+    if(answerTextBoxes.length == 0) {
+        alert("There are no answers to validate.");
+    }
+    else {
+        var percent = (correct / total) * 100;
+        percent = parseFloat(percent).toFixed(2);
+        
+        var spanClass = "";
+        if(percent > 80) {
+            spanClass = "correct-answer"
+        }
+        else if(percent < 60) {
+            spanClass = "wrong-answer";
+        }
+        
+        $('#score-display').html(correct + " / " + total + " &nbsp; | &nbsp; " +
+            "<span class=\"" + spanClass + "\">"+ percent + " %</span>").show();
+    }
+        
+    $("body").css("cursor", "default");
 }
